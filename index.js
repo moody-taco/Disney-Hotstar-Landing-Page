0901d4744d79671e9d3cf9083efeef10f7bc90a4
moody-taco <117022887+moody-taco@users.noreply.github.com>
2022-10-30 20:56:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,130 @@
+let movies = [
+    {
+      name: "The Falcon and the Winter Soldier",
+      cat: "Superhero · 2021",
+      des:
+        "Following the events of “Avengers: Endgame,” Sam Wilson and Bucky Barnes team up in a global adventure that tests their abilities and their patience.",
+      image: "images/slider 2.PNG"
+    },
+    {
+      name: "Loki",
+      cat: "Superhero · Marvel",
+      des:
+        "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”",
+      image: "images/slider 1.PNG"
+    },
+    {
+      name: "WandaVision",
+      cat: "Superhero · Marvel",
+      des:
+        "Wanda Maximoff and Vision—two super-powered beings living idealized suburban lives—begin to suspect that everything is not as it seems.",
+      image: "images/slider 3.PNG"
+    },
+    {
+      name: "Spiderman: Homecoming",
+      cat: "Superhero · 2017",
+      des:
+        "A young Peter Parker/Spider-Man navigates his newfound identity as the web-slinging superhero.",
+      image: "images/slider 4.png"
+    },
+    {
+      name: "Star wars: the phantom menace",
+      cat: "Science Fiction · U/A 13+",
+      des:
+        "Two Jedi Knights set out to search for someone who can bring peace to the Force as the Sith returns to stake claim to the Force.",
+      image: "images/slider 5.png"
+    }
+  ];
+  
+  const carousel = document.querySelector(".carousel");
+  let sliders = [];
+  
+  let slideIndex = 0; // to track current slide index.
+  
+  const createSlide = () => {
+    if (slideIndex >= movies.length) {
+      slideIndex = 0;
+    }
+  
+    // creating DOM element
+    let slide = document.createElement("div");
+    var imgElement = document.createElement("img");
+    let content = document.createElement("div");
+    let h1 = document.createElement("h1");
+    let h3 = document.createElement("h3");
+    let p = document.createElement("p");
+  
+    // attaching all elements
+    imgElement.appendChild(document.createTextNode(""));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    h3.appendChild(document.createTextNode(movies[slideIndex].cat));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(h3);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousel.appendChild(slide);
+  
+    // setting up image
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+  
+    // setting elements classname
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    h3.className = "movie-cat";
+    p.className = "movie-des";
+  
+    sliders.push(slide);
+  
+    if (sliders.length) {
+      sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${
+        30 * (sliders.length - 2)
+      }px)`;
+    }
+  };
+  
+  for (let i = 0; i < 3; i++) {
+    createSlide();
+  }
+  
+  setInterval(() => {
+    createSlide();
+  }, 3000);
+  
+  //Video Cards
+  
+  const videoCards = [...document.querySelectorAll(".video-card")];
+  
+  videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      let video = item.children[1];
+      video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      let video = item.children[1];
+      video.pause();
+    });
+  });
+  
+  //card sliders
+  
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+  
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+  
+    preBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
+  
